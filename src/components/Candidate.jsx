2b78691d2ab2aa8c8fb6f5840c130c8cc7b05dd6
rejennylo <rejennylo@gmail.com>
2023-12-29 14:20:03 +0800

    feat: Dynamic stack chart and candidate data based on year and city

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -1,16 +1,16 @@
 import { StackedBarChart } from "../components/charts/StackedBarChart";
 import { DonutChart } from "./charts/DonutChart";
 
-const Card = ({ image, party, name, votes, color }) => {
+const Card = ({ image, partyName, name, votes, bgColor }) => {
   return (
     <li name="card" className="mb-3 flex">
       <span
-        className={`h-[68px] w-[68px] overflow-hidden rounded-2xl ${color}`}
+        className={`h-[68px] w-[68px] overflow-hidden rounded-2xl ${bgColor}`}
       >
         <img src={image} alt="image" className="mt-1 h-auto w-auto" />
       </span>
       <span className="ml-3 flex flex-col">
-        <span className="text-sm text-secondary-gray">{party}</span>
+        <span className="text-sm text-secondary-gray">{partyName}</span>
         <span>{name}</span>
         <span>{parseInt(votes, 10).toLocaleString()} 票</span>
       </span>
@@ -18,11 +18,7 @@ const Card = ({ image, party, name, votes, color }) => {
   );
 };
 
-const Candidate = ({
-  votesPercentageData,
-  isVoteData,
-}) => {
-
+const Candidate = ({ isTotalVoteData, isVotesData }) => {
   return (
     <section
       name="candidate"
@@ -37,21 +33,21 @@ const Candidate = ({
           className="mb-3 rounded-xl bg-white p-5 lg:mb-0 lg:w-1/2"
         >
           <ul name="candidate-cards" className="mb-5 lg:flex lg:justify-around">
-            {votesPercentageData.map((data) => {
+            {isVotesData.map((data) => {
               return (
                 <Card
                   key={data.name}
                   image={data.image}
-                  party={data.party}
+                  partyName={data.partyName}
                   name={data.name}
                   votes={data.votes}
-                  color={data.bgColor}
+                  bgColor={data.bgColor}
                 />
               );
             })}
           </ul>
           <div className="flex justify-center overflow-hidden rounded-full">
-            <StackedBarChart data={votesPercentageData} />
+            <StackedBarChart data={isVotesData} />
           </div>
         </div>
         <div
@@ -59,11 +55,11 @@ const Candidate = ({
           className="flex justify-between rounded-xl bg-white p-5 lg:w-1/2 lg:justify-start"
         >
           <div className="flex w-auto items-center justify-center">
-            <DonutChart data={parseFloat(isVoteData["投票率"], 10)} />
+            <DonutChart data={parseFloat(isTotalVoteData["投票率"], 10)} />
           </div>
           <ul className="ml-5 w-2/3 lg:flex lg:w-auto lg:flex-wrap">
-            {Object.keys(isVoteData).map((item, i) => {
-              const itemValue = parseInt(isVoteData[item], 10);
+            {Object.keys(isTotalVoteData).map((item, i) => {
+              const itemValue = parseInt(isTotalVoteData[item], 10);
               return (
                 <li
                   key={i}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,7 +5,6 @@ import { MapChart } from "../components/charts/MapChart";
 import {
   partyVotesData,
   partysData,
-  votesPercentageData,
   lineChartData,
   citiesData,
 } from "../data/dummyData";
@@ -78,32 +77,92 @@ const MainPage = () => {
   // 選到的縣市資料
   const cityIs = cities.filter((city) => city[5] === selectedCity);
 
-  // 候選人資料
-  const candidateData = jsonData.elcand[selectedYear].filter(
-    (item) => item[15] !== "Y",
-  );
+  // 圖片資料
+  const colorImageData = [
+    {
+      fillColor: "fill-role-blue",
+      bgColor: "bg-role-blue",
+      image: "./figures/person_vampire_3d_default 1.png",
+      partyNumber: "1",
+    },
+    {
+      fillColor: "fill-role-orange",
+      bgColor: "bg-role-orange",
+      image: "./figures/man_elf_3d_medium-light 1.png",
+      partyNumber: "90",
+    },
+    {
+      fillColor: "fill-role-green",
+      bgColor: "bg-role-green",
+      image: "./figures/troll_3d 1.png",
+      partyNumber: "16",
+    },
+  ];
 
-  // 所有投票概況
-  const votesData = jsonData.elprof[selectedYear].filter(
-    (item) => item[2] === "00" && item[3] === "000",
-  );
+  // 政黨基本資料(elpaty)：
+  const partyData = jsonData.elpaty[year].map((item) => {
+    return { partyNumber: item[0], partyName: item[1] };
+  });
+
+  // 候選人資料
+  const candidateData = jsonData.elcand[selectedYear]
+    .filter((item) => item[15] !== "Y")
+    .map((nameItem) => {
+      const partyItem = partyData.find(
+        (partyItem) => partyItem.partyNumber === nameItem[7],
+      );
+      const colorImage = colorImageData.filter(
+        (colorItem) => colorItem.partyNumber === nameItem[7],
+      )[0];
+      return {
+        ...partyItem,
+        ...colorImage,
+        number: nameItem[5],
+        name: nameItem[6],
+      };
+    });
 
   // 縣市投票概況
-  const cityVoteData = votesData.filter(
-    (item) =>
-      item[0] === cityIs[0][0] &&
-      item[1] === cityIs[0][1] &&
-      item[2] === cityIs[0][2] &&
-      item[3] === cityIs[0][3],
-  );
+  const cityVoteData = jsonData.elprof[selectedYear]
+    .filter((item) => item[2] === "00" && item[3] === "000")
+    .filter(
+      (item) =>
+        item[0] === cityIs[0][0] &&
+        item[1] === cityIs[0][1] &&
+        item[2] === cityIs[0][2] &&
+        item[3] === cityIs[0][3],
+    );
 
   // 投票率
-  const isVoteData = {
+  const isTotalVoteData = {
     投票數: cityVoteData[0][8],
     投票率: cityVoteData[0][17],
     有效票數: cityVoteData[0][6],
     無效票數: cityVoteData[0][7],
   };
+
+  // 縣市候選人得票
+  const getVotesCityData = jsonData.elctks[selectedYear]
+    .filter((item) => item[2] === "00" && item[3] === "000")
+    .filter(
+      (item) =>
+        item[0] === cityIs[0][0] &&
+        item[1] === cityIs[0][1] &&
+        item[2] === cityIs[0][2] &&
+        item[3] === cityIs[0][3],
+    );
+
+  // 整理後的年度總統得票資料
+  const isVotesData = getVotesCityData.map((data, i) => {
+    return {
+      ...candidateData[i],
+      votes: data[7],
+      value: data[8],
+      selected: data[9],
+    };
+  });
+  // 候選人資料(elcand)：[6]候選人名字 [7]政黨代號 [14]當選與否 [15]是否為副手(Y就排除)
+  console.log(candidateData);
 
   return (
     <div className="flex h-screen flex-col">
@@ -122,8 +181,8 @@ const MainPage = () => {
         <div className="flex-grow overflow-auto ">
           <main className="gap-5 px-5 pt-5">
             <Candidate
-              isVoteData={isVoteData}
-              votesPercentageData={votesPercentageData}
+              isTotalVoteData={isTotalVoteData}
+              isVotesData={isVotesData}
             />
             <PartyVotes
               partyVotesData={partyVotesData}
